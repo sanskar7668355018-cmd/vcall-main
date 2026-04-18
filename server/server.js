491acf635503c0ee5d8f87206de7c3a3001479ce
Sanskar Varshney <Sanskar7668355018@gmail.com>
2026-04-19 01:40:38 +0530

    fix: update CORS origin to allow vercel

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 // ✅ 1. CORS FIRST (VERY IMPORTANT)
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["https://vcall-main.vercel.app", "http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
